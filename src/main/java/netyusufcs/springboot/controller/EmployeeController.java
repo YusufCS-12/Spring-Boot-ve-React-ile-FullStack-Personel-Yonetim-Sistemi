bb28d53cef9f2ac2731e3e02e035c3db95fe0ba9
yusuf CS <yusufcs1234@gmail.com>
2021-05-10 15:53:48 +0300

    Spring Boot işlemleri Gün 2, Restfull Service işi yapıldı

--- src/main/java/netyusufcs/springboot/controller/EmployeeController.java
+++ src/main/java/netyusufcs/springboot/controller/EmployeeController.java
@@ -1,7 +1,14 @@
 package netyusufcs.springboot.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import netyusufcs.springboot.model.Employee;
+import netyusufcs.springboot.repository.EmployeeRepository;
 
 /*@RestController bir Spring uygulamasında
  bir Rest-ful denetleyicisi oluşturmak için
@@ -20,5 +27,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/V1")
 public class EmployeeController {
+	/*
+@Autowired temel olarak Dependecy Injection mantığıyla gelmiştir.
+ Bu annatotion ile bir bean spring framework kontrolünde
+başka bir sınıfa injectedilir.
+	 */
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	//get all employees
+	
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployees() {
+		return employeeRepository.findAll();
+	}
 
 }
